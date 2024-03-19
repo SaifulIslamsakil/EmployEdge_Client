df4b7d478317c2988d76f8febf49294b293dde93
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-03-19 21:56:51 +0600

    added to logout function

--- src/Components/DashNavbar/DashNavbar.jsx
+++ src/Components/DashNavbar/DashNavbar.jsx
@@ -1,21 +1,43 @@
-
 import { IoMdNotifications } from "react-icons/io";
-import Logo from "../../assets/logo (2).png"
 import { LuMenu } from "react-icons/lu";
-import { FaUserCircle } from "react-icons/fa";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/Provider";
-const DashNavbar = ({setDasSiderbarToggel, dasSidebarToggel}) => {
-
-    const {user} = useContext(AuthContext)
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+const DashNavbar = ({ setDasSiderbarToggel, dasSidebarToggel }) => {
+    const { user, LogoutUser } = useContext(AuthContext)
+    const Navigate = useNavigate()
+    const handelLogout = () => {
+        LogoutUser()
+            .then(res => {
+                if (res) {
+                    Swal.fire({
+                        title: "Good job!",
+                        text: "Your Account succefully logOut!",
+                        icon: "success"
+                    });
+                    Navigate("/")
+                }
+            })
+            .catch(err => {
+                if (err) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Something went wrong!",
+                    });
+                }
+            })
+    }
     return (
         <nav className="bg-white p-5 w-full flex justify-between items-center">
             <div >
-                <span onClick={()=> setDasSiderbarToggel(!dasSidebarToggel)} className=" z-50  top-8 left-4 text-3xl absolute lg:static"><LuMenu /></span>
+                <span onClick={() => setDasSiderbarToggel(!dasSidebarToggel)} className=" z-50  top-8 left-4 text-3xl absolute lg:static"><LuMenu /></span>
             </div>
             <div className=" flex items-center gap-5 ">
                 <p className="text-2xl " ><IoMdNotifications /></p>
                 <p className=" flex items-center gap-2 text-xl bg-slate-100 p-2 rounded-lg">{user?.displayName}<span className=" text-2xl"><img className=" w-10 h-10 rounded-full" src={user?.photoURL} alt="" /></span></p>
+                <p onClick={handelLogout} className="text-white bg-blue-800 rounded-lg p-3 hover:bg-blue-600  " >logout </p>
             </div>
         </nav>
     );
